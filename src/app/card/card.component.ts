f0109d84c3156f4ad7337cdb7a276e2d1ab0eee3
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-02 23:27:13 +0200

    Card components for home page with plain gsap animation

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewChild,
+  ElementRef,
+  ViewChildren,
+} from "@angular/core";
+import { TweenLite, TweenMax, Linear } from "gsap";
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  selector: "app-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.scss"],
 })
 export class CardComponent implements OnInit {
-
   @Input() title: string;
   @Input() imgUrl: string;
   @Input() imgAlt: string;
 
-  constructor() { }
+  @ViewChildren("card", { read: ElementRef }) card;
 
-  ngOnInit() {
+  private cardNativeElement;
+
+  private tl: TweenLite;
+  private tween;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit(): void {
+    this.cardNativeElement = this.card.first.nativeElement;
+    this.tl = TweenLite.to(this.cardNativeElement,
+      .3,
+      {
+      scale: 1.05,
+    }).reverse();
   }
 
+  onHoverCard() {
+    if (this.tl.reversed()) this.tl.play();
+  }
+
+  offHoverCard() {
+    if (!this.tl.reversed()) this.tl.reverse();
+  }
 }
