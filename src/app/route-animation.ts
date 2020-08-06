3b81e70ff48a66ac3a80f289dc00e28128de5d0e
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-06 20:48:33 +0200

    Refactor: Title cards font size

--- src/app/route-animation.ts
+++ src/app/route-animation.ts
@@ -20,13 +20,27 @@ export const slideInAnimation =
           width: '100%'
         })
       ]),
-      query(':enter', [
-        style({ left: '-100%' })
-      ]),
       query(':leave', animateChild()),
       group([
         query(':leave', [
           animate('400ms ease-in', style({ left: '100%' }))
+        ])
+      ])
+    ]),
+    transition('CasePage => HomePage', [
+      style({ position: 'relative' }),
+      query(':enter', [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: '100%',
+          width: '100%'
+        }),
+        query(':enter', animateChild()),
+        group([
+          query(':enter', [
+            animate('400ms ease-in', style({ left: 0 }))
+          ])
         ])
       ])
     ])
