48ca36081f8ea66fec818b0f6c24466488387e3e
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-09 22:17:04 +0200

    Finished: List animation case

--- src/app/animation.ts
+++ src/app/animation.ts
@@ -2,7 +2,9 @@ import {
   trigger,
   transition,
   animate,
-  style
+  style,
+  query,
+  stagger
 } from "@angular/animations";
 
 export const fadeInAnimation = trigger("fadeIn", [
@@ -12,3 +14,23 @@ export const fadeInAnimation = trigger("fadeIn", [
   ]),
   transition(":leave", [animate("0s", style({ opacity: 0 }))]),
 ]);
+
+export const filterAnimation = trigger('filterAnimation', [
+  transition(':enter, * => 0, * => -1', []),
+  transition(':increment', [
+    query(':enter', [
+      style({ opacity: 0, width: '0px' }),
+      stagger(50, [
+        animate('300ms ease-in', style({ opacity: 1, width: '*' })),
+      ]),
+    ], { optional: true })
+  ]),
+  transition(':decrement', [
+    query(':leave', [
+      stagger(50, [
+        animate('300ms ease-out', style({ opacity: 0, width: '0px' })),
+      ]),
+    ])
+  ]),
+])
+
--- src/app/list-animation/list-animation.component.ts
+++ src/app/list-animation/list-animation.component.ts
@@ -1,22 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { fadeInAnimation, filterAnimation } from "../animation";
 
 @Component({
-  selector: 'app-list-animation',
-  templateUrl: './list-animation.component.html',
-  styleUrls: ['./list-animation.component.scss']
+  selector: "app-list-animation",
+  templateUrl: "./list-animation.component.html",
+  styleUrls: ["./list-animation.component.scss"],
+  animations: [filterAnimation],
 })
 export class ListAnimationComponent implements OnInit {
-  criteria  = new FormControl('');
+  criteria = new FormControl("");
+  items: string[] = [];
+  visibleItems: string[] = [];
+  totalItems = -1;
 
-  constructor() { }
+  constructor() {
+    this.items.push("Item 1");
+    this.items.push("Lettuce");
+    this.items.push("Pineapple");
+    this.items.push("Black pepper");
+    this.items.push("Item 5");
+    this.items.push("Pear");
+    this.items.push("Spinach");
 
-  ngOnInit() {
+    this.visibleItems = this.items;
   }
 
-  applyCriteria () {
-    console.log(this.criteria .value.trim());
+  ngOnInit() {}
 
+  applyCriteria() {
+    const cleanCriteria = this.criteria.value
+      ? this.criteria.value.toLowerCase().trim()
+      : "";
+    console.log(cleanCriteria);
+
+    if (cleanCriteria !== "")
+      this.visibleItems = this.items.filter((item) =>
+        item.toLowerCase().includes(cleanCriteria)
+      );
+    else this.visibleItems = this.items;
+
+    const newTotalItems = this.visibleItems.length;
+
+    if (newTotalItems !==  this.totalItems)
+      this.totalItems = newTotalItems;
   }
-
 }
