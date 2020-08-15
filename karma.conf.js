cdc55b5efa2e3c094f82bd532e9b46f7d965d0d2
rafael.guardeno <rafael.guardeno.marquez@gmail.com>
2020-08-15 06:57:46 +0200

    Change repository's name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/greensock-showcases'),
+      dir: require('path').join(__dirname, './coverage/animations-showcases'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
